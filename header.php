d5f65070379c2a070bfe40ca9fe20bbb21623c98
Duc Truong <phuduc1193@gmail.com>
2017-01-19 21:10:12 +0000

    DB config

--- header.php
+++ header.php
@@ -1,3 +1,7 @@
+<?php
+  include 'config.php';
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,7 +9,7 @@
   <meta name="keywords" content="leadclickz">
   <meta name="author" content="Duc Truong">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Leadclickz Admin</title>
+  <title>Leadclickz Dashboard</title>
   
   <!-- Style -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
