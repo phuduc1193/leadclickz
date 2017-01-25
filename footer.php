4696a674ac2ba44e1fc16ddfd9f6079ebf84dcea
Duc Truong <phuduc1193@gmail.com>
2017-01-25 05:28:08 +0000

    Optimize all css and js vendors

--- footer.php
+++ footer.php
@@ -6,25 +6,8 @@
 <!-- ./wrapper -->
 
   <!-- Scripts -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.3/jquery.min.js"></script>
-  <script src="http://code.jquery.com/ui/1.12.0/jquery-ui.min.js"></script>
-  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-  <script>
-    $.widget.bridge('uibutton', $.ui.button);
-  </script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
-  <script src="plugins/morris/morris.min.js"></script>
-  <script src="plugins/sparkline/jquery.sparkline.min.js"></script>
-  <script src="plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
-  <script src="plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
-  <script src="plugins/knob/jquery.knob.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
-  <script src="plugins/daterangepicker/daterangepicker.js"></script>
-  <script src="plugins/datepicker/bootstrap-datepicker.js"></script>
-  <script src="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
-  <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
-  <script src="plugins/fastclick/fastclick.js"></script>
+  <script src="assets/js/vendors.min.js"></script>
+
   <!-- AdminLTE App -->
   <script src="assets/js/app.min.js"></script>
   <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
